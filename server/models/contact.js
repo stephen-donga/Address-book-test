d165cbf963a49f06a26f2d55b75c198ab1e0ef47
stephen-edonga11 <stephenedonga@gmail.com>
2020-11-29 12:02:41 +0300

    Created address book project and setting up folder structure and backend in progress ...

---
+++ server/models/contact.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    username:{
+        type: String,
+         required: true,
+         maxlength:20,
+         trim: true
+        },
+    phone: {
+        type: Number,
+         required: true,
+        },
+    address: {
+        type: String,
+         required: true, 
+         maxlength:50,
+         trim:true
+        },
+},{timestamps:true});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+module.exports = Contact;
